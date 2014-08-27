5174bfee80cf7c5434155da9a52ef0291214f794
king <471897134@qq.com>
2014-08-27 22:52:57 +0800

    init event model

--- public/js/script.js
+++ public/js/script.js
@@ -3,5 +3,22 @@ require.config({
 });
 
 require(['event'], function (event){
-	console.log(event.addEvent);
+	
+	var doc = document,
+		ul = doc.querySelector('ul'),
+		list = doc.querySelectorAll('li');
+
+	event.addEvent(ul, 'click', function(e){
+		var target = event.getTarget(e);
+		alert(target.tagName);
+	});
+
+	event.addEvent(list[0], 'click', function(e){
+		e = event.getEvent(e);
+		var target = event.getTarget(e);
+		alert(target.tagName);
+		event.stopPropagation(e);
+		event.preventDefault(e);
+	});
+
 });
--- public/king/event.js
+++ public/king/event.js
@@ -1,5 +1,14 @@
 define(function(){
-	var addEvent = window.addEventListener? 
+	
+	var addEvent,
+		removeEvent,
+		getEvent,
+		getTarget,
+		stopPropagation,
+		preventDefault,
+		on;
+
+	addEvent = window.addEventListener? 
 		function (elem, type, handler){
 			elem.addEventListener(type, handler, false);
 		}:
@@ -7,9 +16,51 @@ define(function(){
 			elem.attachEvent('on'+type, function(){
 				handler.call(elem, window.event);
 			})
+		};
+
+	removeEvent = window.removeEventListener?
+		function (elem, type){
+			elem.removeEventListener(type);
+		}:
+		function (elem, type){
+			elem.detachEvent('on'+type);
+		};
+
+	getEvent = function(e){
+		return e||window.event;
+	};
+
+	getTarget = function(e){
+		return e.target || e.srcElement;
+	};
+
+	stopPropagation = function(e){
+		if (e.stopPropagation){
+			e.stopPropagation();
+		}else{
+			e.cancelBubble = true;
 		}
+	};
+
+	preventDefault = function(e){
+		if (e.preventDefault){
+			e.preventDefault();
+		}else{
+			e.returnValue = false;
+		}
+	}
+
+	on = function(elem, type, childSelctor, handler){
+		
+	}
+
 
 	return {
-		addEvent: addEvent
+		addEvent: addEvent,
+		removeEvent: removeEvent,
+		getEvent: getEvent,
+		getTarget: getTarget,
+		stopPropagation: stopPropagation,
+		preventDefault: preventDefault
 	}
 })
