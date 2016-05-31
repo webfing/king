637d5b3d23d70499e55bc3dadc40473580b99955
king <471897134@qq.com>
2016-05-31 22:50:32 +0800

    add route.book.add

--- routes/book.js
+++ routes/book.js
@@ -6,5 +6,8 @@ router.get('/book', function(req, res) {
   res.render('book', { title: 'Express' });
 });
 
+router.get('/book/add', function(req, res){
+	res.render('book', {title: 'book'});
+});
 
 module.exports = router;
