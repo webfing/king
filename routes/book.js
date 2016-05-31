97320e79c44225c73a819322352baff4aa9feda8
tljtong <tljtong@163.com>
2016-05-31 14:47:00 +0000

    edit route.book

--- routes/book.js
+++ routes/book.js
@@ -2,12 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/book', function(req, res) {
+  res.render('book', { title: 'Express' });
 });
 
-router.get('/test', function(req, res) {
-  res.render('test', { title: 'Express' });
-});
 
 module.exports = router;
